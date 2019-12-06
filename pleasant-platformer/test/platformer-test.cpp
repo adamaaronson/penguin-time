9e478c0658c049fb969ce2928a0dd6e64349613c
adamaaronson <adamaaronson@gmail.com>
2019-12-06 10:44:00 -0600

    Updated spritesheet again

---
+++ pleasant-platformer/test/platformer-test.cpp
@@ -0,0 +1,8 @@
+//
+//  platformer-test.cpp
+//  pleasant-platformer
+//
+//  Created by Adam Aaronson on 12/5/19.
+//
+
+#include <stdio.h>
---
+++ pleasant-platformer/test/tests-main.cpp
@@ -0,0 +1,8 @@
+//
+//  tests-main.cpp
+//  pleasant-platformer
+//
+//  Created by Adam Aaronson on 12/5/19.
+//
+
+#include <stdio.h>
