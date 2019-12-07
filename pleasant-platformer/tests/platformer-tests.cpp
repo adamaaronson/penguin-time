b4741bae2331bbca7b5025fbf5f2e702590473c8
adamaaronson <adamaaronson@gmail.com>
2019-12-07 17:44:22 -0600

    Added Catch2

---
+++ pleasant-platformer/tests/platformer-tests.cpp
@@ -0,0 +1,17 @@
+//
+//  platformer-tests.cpp
+//  pleasant-platformer-tests
+//
+//  Created by Adam Aaronson on 12/7/19.
+//
+
+#include <stdio.h>
+#include "player.hpp"
+#include "block.hpp"
+#include "enemy.hpp"
+#include "level.hpp"
+#include "catch.hpp"
+
+TEST_CASE("Test test") {
+    REQUIRE(1 == 1);
+}
---
+++ pleasant-platformer/tests/tests-main.cpp
@@ -0,0 +1,10 @@
+//
+//  tests-main.cpp
+//  pleasant-platformer-tests
+//
+//  Created by Adam Aaronson on 12/7/19.
+//
+
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_COMPILE_FAST
+#include "catch.hpp"
